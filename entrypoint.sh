0842bbf41899ace0ecdeac75f239f99443ccfe31
Hill <hillyu@live.com>
2021-12-17 17:30:46 +0800

    dockerd start with sudo

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ git clone --recursive https://github.com/hillyu/hill.git dotfiles \
 #sed -i 's/bash/zsh/g' /etc/passwd
 if [ "$#" -eq 0 ] || [ "${1#-}" != "$1" ]; then
 	# add our default arguments
-	set -- dockerd \
+	set -- sudo dockerd \
 		--host=unix:///var/run/docker.sock \
 		--host=tcp://0.0.0.0:2375 \
 		"$@"
